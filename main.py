0377c524ad945b44abca3980ea9f1aee96b3a786
vando <vanderson.castro@ufv.br>
2021-03-27 17:23:30 -0300

    Initial commit, criação do projeto do curso

---
+++ main.py
@@ -0,0 +1,12 @@
+# Curso de Python em 6 horas.
+
+def print_Curso(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f' Curso de {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_Curso('Python em 6 Horas')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
