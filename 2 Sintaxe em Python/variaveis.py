1c3395f0e07fac28bb1c14b86d35c0e82b474488
Vandersonn <vanderson.castro@ufv.br>
2021-03-28 09:11:44 -0300

    subindo exemplos

---
+++ 2 Sintaxe em Python/variaveis.py
@@ -0,0 +1,21 @@
+# variaveis em python
+
+#Strings
+name = 'Genocidas vivem mortos matando os vivos.'
+print(name)
+
+#Float
+number =12.5
+print(number)
+
+#inteiro
+number1 = 12
+print(number1)
+
+#boolean
+a = True or False
+print(a)
+
+#Complexo
+numero_complexo = 12+5j
+print(numero_complexo)
